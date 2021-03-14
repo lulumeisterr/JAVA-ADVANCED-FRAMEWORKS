718cc43dd57f9a81016015f33c8a0718de5661fb
lulumeisterr <lucasrodriguesdonascimento@outlook.com>
2021-03-14 14:16:36 -0300

    usando Rules do junit para listar todos os possiveis erros

--- StrategyPatterns/src/java/br/com/gof/impostos/CalcularImpostoTest.java
+++ StrategyPatterns/src/java/br/com/gof/impostos/CalcularImpostoTest.java
@@ -7,19 +7,24 @@ import java.math.BigDecimal;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.com.gof.orcamento.Orcamento;
 
 public class CalcularImpostoTest {
 
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
 	public void teste() {
 
 		/**
 		 * FIRST
 		 * F = Fast
-		 * I = Independente (Um teste nao pode depender de outro
+		 * I = Independente (Um teste nao pode depender de outro) // Isolado
 		 * R = Repetitivel (Um teste pode ser executado N vezes)
 		 * S = Auto verificado ( Um teste deve saber quando falha e quando funciona)
 		 * T = Timely
@@ -42,13 +47,20 @@ public class CalcularImpostoTest {
 		// Verificacao usando lib Hamcrest
 
 		// Usando metodo (is) static
-		Assert.assertThat(new BigDecimal("10.0"),is(equalTo(resultado)));
+		//Assert.assertThat(new BigDecimal("10.0"),is(equalTo(resultado)));
+		
+		error.checkThat(new BigDecimal("10.0"),is(equalTo(resultado)));
 		
 		//Negacao
-		Assert.assertThat(resultado,CoreMatchers.is(CoreMatchers.not(new BigDecimal("10.1"))));
+		//Assert.assertThat(resultado,CoreMatchers.is(CoreMatchers.not(new BigDecimal("10.1"))));
 		
-		Assert.assertThat(orcamento.getValor(),CoreMatchers.is(CoreMatchers.not("0")));
+		error.checkThat(resultado,CoreMatchers.is(CoreMatchers.not(new BigDecimal("10.5"))));
+		
+		
+		//Assert.assertThat(orcamento.getValor(),CoreMatchers.is(CoreMatchers.not("0")));
 		//Assert.assertTrue(!orcamento.getValor().equals(new BigDecimal("0")));
+		
+		error.checkThat(orcamento.getValor(),CoreMatchers.is(CoreMatchers.not("0")));
 
 	}
 
